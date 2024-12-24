44dadcf9e83b003aea99aac4d13efbc93dfc20c9
David <150817765+minhlt246@users.noreply.github.com>
2024-12-24 13:42:54 +0700

    pushj

---
+++ freetds-1.4.24/doc/reference/search/all_b.js
@@ -0,0 +1,15 @@
+var searchData=
+[
+  ['manual_0',['FreeTDS Reference Manual',['../index.html',1,'']]],
+  ['md4context_1',['MD4Context',['../a01350.html',1,'']]],
+  ['md5context_2',['MD5Context',['../a01354.html',1,'']]],
+  ['memory_20allocation_3',['Memory allocation',['../a00577.html',1,'']]],
+  ['metacomp_4',['METACOMP',['../a00594.html',1,'']]],
+  ['metadata_5',['METADATA',['../a00586.html',1,'']]],
+  ['metadata_5ft_6',['metadata_t',['../a00658.html',1,'']]],
+  ['minute_7',['minute',['../a01186.html#a81d1931139497023deb1bb75dc0aae7a',1,'tdsdaterec']]],
+  ['money_20functions_8',['Money functions',['../a00567.html',1,'']]],
+  ['month_9',['month',['../a01186.html#af1542b5698f668f7df650b20fa27c177',1,'tdsdaterec']]],
+  ['mpz_5ft_10',['mpz_t',['../a00798.html',1,'']]],
+  ['msdblib_11',['msdblib',['../a01002.html#af3aab1ff30d324143f440bcd9f60d1c3',1,'tds_dblib_dbprocess']]]
+];
